fb2a6e16fceeaf01c051452b42fe96c07049ed45
TiagoDiass <tiago.costadiasss@gmail.com>
2021-04-29 21:41:24 -0300

    chore: add static props to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
-export default function Home() {
+export default function Home(props) {
   return (
     <>
-      <h1>Página index</h1>
+      <h1>Página index </h1>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const episodes = response.json();
+
+  return {
+    props: { episodes },
+    revalidate: 60 * 60 * 8,
+  };
 }
