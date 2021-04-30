b03d250b2f91a872aad1a2bdc2442fe0361d0ebd
TiagoDiass <tiago.costadiasss@gmail.com>
2021-04-29 21:21:06 -0300

    feat: add player component(empty state only)

--- src/components/Player/Player.styles.ts
+++ src/components/Player/Player.styles.ts
@@ -12,3 +12,85 @@ export const PlayerContainer = styled.aside`
   align-items: center;
   justify-content: space-between;
 `;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  column-gap: 1rem;
+
+  strong {
+    font-family: Lexend, sans-serif;
+    font-weight: 600;
+  }
+`;
+
+export const EmptyPlayer = styled.div`
+  width: 100%;
+  height: 20rem;
+  border: 1.5px solid var(--purple-300);
+  border-radius: 1.5rem;
+  background: linear-gradient(143.8deg, rgba(145, 100, 250, 0.8) 0%, rgba(0, 0, 0, 0) 100%);
+  padding: 4rem;
+
+  text-align: center;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Footer = styled.footer<{ empty: boolean }>`
+  align-self: stretch;
+
+  opacity: ${props => (props.empty ? '0.5' : '1')};
+`;
+
+export const Progress = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  font-size: 0.875rem;
+
+  span {
+    display: inline-block;
+    width: 4rem;
+    text-align: center;
+  }
+
+  .slider {
+    flex: 1;
+
+    .emptySlider {
+      width: 100%;
+      height: 4px;
+      background: var(--purple-300);
+      border-radius: 2px;
+    }
+  }
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2.5rem;
+  gap: 1.5rem;
+
+  button {
+    background: transparent;
+    border: 0;
+    font-size: 0;
+
+    &.playButton {
+      width: 4rem;
+      height: 4rem;
+      border-radius: 1rem;
+      background: var(--purple-300);
+      transition: filter 0.15s ease;
+
+      &:hover {
+        filter: brightness(0.97);
+      }
+    }
+  }
+`;
--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,5 +1,48 @@
 import * as S from './Player.styles';
 
 export default function Player() {
-  return <S.PlayerContainer></S.PlayerContainer>;
+  return (
+    <S.PlayerContainer>
+      <S.Header>
+        <img src='/images/playing.svg' alt='Tocando agora' />
+        <strong>Tocando agora</strong>
+      </S.Header>
+
+      <S.EmptyPlayer>
+        <strong>Selecione um podcast para ouvir</strong>
+      </S.EmptyPlayer>
+
+      <S.Footer empty={true}>
+        <S.Progress>
+          <span>00:00</span>
+          <div className='slider'>
+            <div className='emptySlider' />
+          </div>
+          <span>00:00</span>
+        </S.Progress>
+
+        <S.Buttons>
+          <button type='button'>
+            <img src='/images/shuffle.svg' alt='Embaralhar' />
+          </button>
+
+          <button type='button'>
+            <img src='/images/play-previous.svg' alt='Tocar anterior' />
+          </button>
+
+          <button type='button' className='playButton'>
+            <img src='/images/play.svg' alt='Tocar' />
+          </button>
+
+          <button type='button'>
+            <img src='/images/play-next.svg' alt='Tocar próxima' />
+          </button>
+
+          <button type='button'>
+            <img src='/images/repeat.svg' alt='Repetir' />
+          </button>
+        </S.Buttons>
+      </S.Footer>
+    </S.PlayerContainer>
+  );
 }
