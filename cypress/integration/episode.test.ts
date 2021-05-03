6d298451d09ecc55d65bc58c04c9101e00a9cdd4
TiagoDiass <tiago.costadiasss@gmail.com>
2021-05-02 23:00:12 -0300

    test: ensure Episode page plays the episode when user clicks on play button

--- cypress/integration/episode.test.ts
+++ cypress/integration/episode.test.ts
@@ -22,7 +22,7 @@ describe('Episode page', () => {
   });
 
   beforeEach(() => {
-    cy.viewport(1530, 730);
+    cy.viewport(1530, 950);
     cy.visit(`/episodes/${episodes[1].id}`);
   });
 
@@ -32,13 +32,33 @@ describe('Episode page', () => {
     cy.findByRole('button', { name: /voltar/i }).should('exist');
   });
 
-  it('should redirect user to the home page when clicks on logo', () => {
+  it('should redirect user to home page when clicks on logo', () => {
     cy.findByRole('img', { name: /podcastr/i }).click();
     cy.url().should('equal', `${baseUrl}/`);
   });
 
-  it('should redirect user to the home page when clicks on back button', () => {
+  it('should redirect user to home page when clicks on back button', () => {
     cy.findByRole('button', { name: /voltar/i }).click();
     cy.url().should('equal', `${baseUrl}/`);
+  });
+
+  it('should play the episode when user clicks on the play button', () => {
+    // ensure player is empty
+    cy.get('aside').within(() => {
+      cy.findByText(/selecione um podcast para ouvir/i).should('exist');
+      cy.findAllByRole('button').should('have.length', 5);
+      cy.findAllByRole('button').should('be.disabled');
+    });
+
+    // click on play button
+    cy.findByRole('button', { name: /tocar episódio/i }).click();
+
+    // ensure player is correct
+    cy.get('aside').within(() => {
+      cy.get('div>strong').should('contain.text', episodes[1].title);
+      cy.findAllByRole('button').should('be.enabled');
+      cy.findByRole('button', { name: /tocar/i }).should('not.exist');
+      cy.findByRole('button', { name: /pausar/i }).should('exist');
+    });
   });
 });
